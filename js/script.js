d28c05892c483aae5a346fb668a53e9fe0fab353
fitse01 <135918799+fitse01@users.noreply.github.com>
2023-11-21 19:56:47 +0300

    JS practice with webiesBootCamp

---
+++ js/script.js
@@ -0,0 +1,72 @@
+//💻 Day 1: Exercises
+
+/* Write a single line comment which says, comments can make code readable
+//comments can make code readable.
+*/
+
+//Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript.
+
+
+//Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* comments can make code readable, easy to reuse and informative*/
+
+
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+var stringVariable = "Hello, World!";
+var booleanVariable = true;
+var undefinedVariable = undefined;
+var nullVariable = null;
+
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// Declare variables of different data types
+var stringVariable = "Hello, World!";
+var booleanVariable = true;
+var undefinedVariable = undefined;
+var nullVariable = null;
+var numberVariable = 42;
+var objectVariable = {};
+var arrayVariable = [];
+
+// Use typeof operator to check data types
+console.log(typeof stringVariable); // Output: string
+console.log(typeof booleanVariable); // Output: boolean
+console.log(typeof undefinedVariable); // Output: undefined
+console.log(typeof nullVariable); // Output: object
+console.log(typeof numberVariable); // Output: number
+console.log(typeof objectVariable); // Output: object
+console.log(typeof arrayVariable); // Output: object
+
+//Declare four variables without assigning values
+var name;
+let pi;
+let age;
+var section ;
+
+//Declare four variables with assigned values
+var name = 'Fitsum';
+let piValue = 3.14;
+let ageOfStudent = 21;
+var studentId = 'ugr/25418/14' ;
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+var firstName = 'Fitsum';
+var lastName = 'Tafese';
+var  maritalStatus = 'single';
+var country = 'Ethiopia';
+var ageOf = 21;
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+var firstName = "Fitsum", lastName = "Tafese", maritalStatus = "Single", country = "Ethiopia", ageOf = 21;
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+const myAge = 25;
+const yourAge = 30;
+console.log ("I am " + myAge + "years old");
+console.log("you are " + yourAge + "years old" );
+
+//I am 25 years old.
+//You are 30 years old.
+
+
+//🎉 CONGRATULATIONS ! 🎉
